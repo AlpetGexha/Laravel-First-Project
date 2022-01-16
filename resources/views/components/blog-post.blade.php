67ac4e859f854dd8649216d8b268e52c4236edfb
Alpet Gexha <agexha@gmail.com>
2022-01-16 01:02:29 +0100

    Change blog post style

--- resources/views/components/blog-post.blade.php
+++ resources/views/components/blog-post.blade.php
@@ -1,51 +1,39 @@
- <div>
-     <div class="drejtimet card shadow bg-light mt-2 mb-5">
-         <div class="d-flex justify-content-center flex-wrap">
-             <div class="row g-0 bg-light container-layout">
-                 <h1 class="mt-0">{{ $title }}</h1>
-                 <div class="col-md-6 mb-md-0 p-md-4">
-                     {{-- <img src="{{ $post->photo }}" class="w-100 " alt="{{ $post->title }}"> --}}
-                     <img src="https://picsum.photos/200/150/?random" class="w-100" alt="{{ $title }}">
-                 </div>
+   {{-- {{ $category }} --}}
+   <section class="my-5">
+       <div class="row">
+           <div class="col-lg-5 col-xl-4">
+               <div class="view overlay rounded z-depth-1-half mb-lg-0 mb-4">
+                   <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/49.webp"
+                       alt="Sample image">
+                   <a>
+                       <div class="mask rgba-white-slight"></div>
+                   </a>
+               </div>
+           </div>
 
-                 <div class="col-md-6 p-4 ps-md-0 ">
-                     <div class="row">
-                         <div class="col">
-                             <a href="{{ route('user.show', $username) }}">
-                                 <p>{{ $username }}</p>
-                             </a>
-                         </div>
-                         <div class="col">
-                             <p class="text-muted">{{ $created_at }}</p>
-                         </div>
-                     </div>
-                     {{-- <livewire:category.postcategory :id="$post->id"> --}}
-                     {{ $category }}
-
-                     <p class="body-cart-text">{{ Str::limit($body, 200, '...') }}.</p>
-
-                     <div class="d-flex bd-highlight mb-3">
-                         <div class="p-2 bd-highlight">
-                             <i class="far fa-eye">{{ $views }}</i>
-                         </div>
-                         <div class="p-2 bd-highlight">
-                             <i class="far fa-comment">{{ $comments }}</i>
-                         </div>
-                         <div class="p-2 bd-highlight">
-                             <i class="far fa-thumbs-up">{{ $likes }}</i>
-                         </div>
-                         <div class="p-2 bd-highlight">
-                             <i class="far fa-bookmark">{{ $shares }}</i>
-                         </div>
-                         <div class="ms-auto p-2 bd-highlight">
-                             <a class="btn btn-outline-success" href="{{ route('post.single', $post_slug) }}">Lexo me shume</a>
-                         </div>
-                     </div>
-                 </div>
-             </div>
-         </div>
-     </div>
-     {{-- <div class="body-footer d-flex justify-content-end">
-             {{ $posts->links() }}
-         </div> --}}
- </div>
+           <div class="col-lg-7 col-xl-8">
+               <h3 class="font-weight-bold mb-3 text-center"><strong>{{ $title }}</strong></h3>
+               <p class="dark-grey-text">{{ Str::limit($body, 261, '...') }}</p>
+               <p>nga <a href="{{ route('user.show', $username) }}" class="font-weight-bold">{{ $username }}</a>,
+                   {{ $created_at }}</p>
+               <div class="d-flex bd-highlight">
+                   <div class="p-2 bd-highlight">
+                       <i class="far fa-eye">{{ $views }}</i>
+                   </div>
+                   <div class="p-2 bd-highlight">
+                       <i class="far fa-comment">{{ $comments }}</i>
+                   </div>
+                   <div class="p-2 bd-highlight">
+                       <i class="far fa-thumbs-up">{{ $likes }}</i>
+                   </div>
+                   <div class="p-2 bd-highlight">
+                       <i class="far fa-bookmark">{{ $shares }}</i>
+                   </div>
+                   <div class="ms-auto p-2 bd-highlight">
+                       <a class="btn btn-outline-success btn-md" href="{{ route('post.single', $post_slug) }}">Lexo më
+                           shume</a>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </section>
