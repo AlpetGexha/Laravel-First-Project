055190a1881dafbfe6f21e8badecbcad57376726
Alpet Gexha <agexha@gmail.com>
2022-01-19 01:04:36 +0100

    Add DarkMode

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,6 +13,7 @@
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="{{ asset('AdminPanel/plugins/fontawesome-free/css/all.min.css') }}">
     <!-- Theme style -->
+    {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
     <link rel="stylesheet" href="{{ asset('AdminPanel/dist/css/adminlte.min.css') }}">
     {{-- Livewire --}}
     <livewire:styles />
@@ -130,37 +131,46 @@
                     <a class="nav-link" data-toggle="dropdown" href="#">
                         <i class="far fa-bell"></i>
                         <span class="badge badge-warning navbar-badge">15</span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                        <span class="dropdown-header">15 Notifications</span>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <i class="fas fa-envelope mr-2"></i> 4 new messages
-                            <span class="float-right text-muted text-sm">3 mins</span>
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <i class="fas fa-users mr-2"></i> 8 friend requests
-                            <span class="float-right text-muted text-sm">12 hours</span>
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <i class="fas fa-file mr-2"></i> 3 new reports
-                            <span class="float-right text-muted text-sm">2 days</span>
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-                    </div>
+
+                        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                            <span class="dropdown-header">15 Notifications</span>
+                            <div class="dropdown-divider"></div>
+                            <a href="#" class="dropdown-item">
+                                <i class="fas fa-envelope mr-2"></i> 4 new messages
+                                <span class="float-right text-muted text-sm">3 mins</span>
+                            </a>
+                            <div class="dropdown-divider"></div>
+                            <a href="#" class="dropdown-item">
+                                <i class="fas fa-users mr-2"></i> 8 friend requests
+                                <span class="float-right text-muted text-sm">12 hours</span>
+                            </a>
+                            <div class="dropdown-divider"></div>
+                            <a href="#" class="dropdown-item">
+                                <i class="fas fa-file mr-2"></i> 3 new reports
+                                <span class="float-right text-muted text-sm">2 days</span>
+                            </a>
+                            <div class="dropdown-divider"></div>
+                            <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+                        </div>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" data-widget="fullscreen" href="#" role="button">
                         <i class="fas fa-expand-arrows-alt"></i>
                     </a>
                 </li>
+
                 <li class="nav-item">
                     <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
                         <i class="fas fa-th-large"></i>
                     </a>
+                </li>
+                <li class="nav-item">
+                    <div class="nav-link">
+                        <div class="custom-control custom-switch">
+                            <input type="checkbox" class="custom-control-input" id="darkMode">
+                            <label class="custom-control-label" for="darkMode">Dark Mode</label>
+                        </div>
+                    </div>
                 </li>
             </ul>
         </nav>
@@ -269,6 +279,7 @@
     <!-- jQuery -->
     <script src="{{ asset('AdminPanel/plugins/jquery/jquery.min.js') }}"></script>
     <!-- Bootstrap 4 -->
+    <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('AdminPanel/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <!-- AdminLTE App -->
     <script src="{{ asset('AdminPanel/dist/js/adminlte.min.js') }}"></script>
@@ -277,7 +288,6 @@
     <script>
         (function($) {
             "use strict";
-
             // Shtoni gjendje aktive në sidebar nav links
             var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
             $(".nav .nav-link").each(function() {
@@ -286,6 +296,65 @@
                 }
             });
         })(jQuery);
+        //Aktivizo Dark Mode
+        let darkModeOn = false;
+
+        const createStorage = (name, value) => {
+            localStorage.setItem(name, value);
+        }
+
+        const readStorage = name => {
+            return localStorage.getItem(name);
+        }
+
+        const deleteStorage = name => {
+            localStorage.removeItem(name);
+        }
+
+        const toggleDarkMode = (e) => {
+            if (document.body.classList.contains("dark-mode")) {
+                document.body.classList.remove("dark-mode");
+                darkModeOn = false;
+                createStorage("my_preferredMode", "light-mode");
+            } else {
+                document.body.classList.add("dark-mode");
+                darkModeOn = true;
+                createStorage("my_preferredMode", "dark-mode");
+            }
+        }
+
+        document.getElementById("darkMode").addEventListener("click", toggleDarkMode)
+
+        document.addEventListener("DOMContentLoaded", () => {
+            if (readStorage("my_preferredMode")) {
+                if (readStorage("my_preferredMode") == "dark-mode") {
+                    darkModeOn = true;
+                } else {
+                    darkModeOn = false;
+                }
+            } else {
+                if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+                    darkModeOn = true;
+                } else {
+                    if (document.body.classList.contains("dark-mode")) {
+                        darkModeOn = true;
+                    } else {
+                        darkModeOn = false;
+                    }
+                }
+            }
+
+            if (darkModeOn) {
+                if (!document.body.classList.contains("dark-mode")) {
+                    document.body.classList.add("dark-mode");
+                }
+                document.getElementById("darkMode").checked = true
+            } else {
+                if (document.body.classList.contains("dark-mode")) {
+                    document.body.classList.remove("dark-mode");
+                }
+            }
+        })
     </script>
 </body>
 
