3aa533e2188f5444f4ba1a122c7d22c9e2aa4681
Alpet Gexha <agexha@gmail.com>
2022-01-19 17:45:45 +0100

    delete dump

--- resources/views/auth/post/saves.blade.php
+++
@@ -1,9 +0,0 @@
-<x-app-layout>
-    <x-slot name="header">
-        Postimet e ruatjura
-    </x-slot>
-    {{-- Postime e ruajtura --}}
-
-    <livewire:post.saves>
-
-</x-app-layout>
--- resources/views/auth/post/show.blade.php
+++
@@ -1,7 +0,0 @@
-<x-app-layout>
-    <x-slot name="header">
-        Postimet
-    </x-slot>
-    {{-- Postime e ruajtura --}}
-
-</x-app-layout>
