a998154704c4f278a25536b20d6ad02e004274df
Alpet Gexha <agexha@gmail.com>
2022-01-28 13:29:04 +0100

    Update CategotyChart.php

--- app/Http/Livewire/Admin/Dashboard/CategotyChart.php
+++ app/Http/Livewire/Admin/Dashboard/CategotyChart.php
@@ -4,28 +4,44 @@ namespace App\Http\Livewire\Admin\Dashboard;
 
 use App\Models\Category;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class CategotyChart extends Component
 {
     public function render()
     {
-        $category = Category::select('id', 'created_at')
-            ->whereBetween(
-                'created_at',
-                [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()]
-            )->get();
+        $total = []; //totali per shikimin e dites(by default = null )
+        $dates = collect(); //renditja e ditëve
 
-        $datas = [0, 0, 0, 0, 0, 0, 0];
-
-        foreach ($category as $category) {
-            $datas[$category->created_at->dayOfWeek] = $category->count();
+        // Shto ditët e javës në array dhe by default jep vlerën 0
+        foreach (range(-6, 0) as $i) {
+            $date = Carbon::now()->endOfWeek($i)->format('Y-m-d');
+            $dates->put($date, 0);
         }
 
-        array_push($datas, array_shift($datas));
+        //Merr shumen e postimeve per ditët e javës
+        $category = Category::where('created_at', '>=', $dates->keys()->first())
+            ->where('is_active', 1)
+            ->groupBy('date')
+            ->orderBy('date')
+            ->get([
+                DB::raw('DATE( created_at ) as date'),
+                DB::raw('COUNT( * ) as "count"')
+            ])
+            ->pluck('count', 'date');
+
+        // Bashko vargjet
+        $dates = $dates->merge($category);
+
+        // Jep rezultatin final (vlerat)
+        foreach ($dates as $date => $count) {
+            $total[] = $count;
+        }
+
 
         return view('livewire.admin.dashboard.categoty-chart', [
-            'datas' => $datas
+            'datas' => $total
         ]);
     }
 }
